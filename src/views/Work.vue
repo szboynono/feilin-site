a6c0d64917ea1561b5688a2447286927935a0a0d
szboynono <szboylbd@hotmail.com>
2020-09-26 19:01:04 -0400

    latests

--- src/views/Work.vue
+++ src/views/Work.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="work grid grid-cols-1 lg:grid-cols-3 grid-rows-4 gap-8">
+  <div class="grid grid-cols-1 lg:grid-cols-3 grid-rows-4 gap-8">
     <div class="lg:col-start-2 sm:col-start-1 lg:col-span-2 sm:col-span-12 gallary-item"></div>
     <div class="lg:col-start-2 sm:col-start-1 lg:col-span-1 sm:col-span-12 gallary-item"></div>
     <div class="gallary-item lg:col-start-3 sm:col-start-2 lg:col-span-1 sm:col-span-12 "></div>
     <div class="lg:col-start-2 sm:col-start-1 lg:col-span-2 sm:col-span-12 gallary-item"></div>
     <div class="lg:col-start-2 sm:col-start-1 lg:col-span-2 sm:col-span-12 gallary-item"></div>
   </div>
-  <div class="grid grid-cols-1 lg:grid-cols-3 grid-row-1 gap-8 mt-8">
+  <div class="grid grid-cols-1 lg:grid-cols-3 grid-row-1 gap-8 mt-16">
     <div class="col-start-2 col-span-2">
       <p class="font-normal">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit modi, eos
@@ -19,7 +19,15 @@
       </p>
     </div>
   </div>
-  <div class="grid grid-col-1 lg:grid-cols-3 grid-row-1 gap-8 my-8">
+  <div class="grid grid-col-1 lg:grid-cols-3 grid-row-1 gap-8 mt-8">
+    <div class="col-start-2 col-span-2 tags">
+      <a href="#" class="font-normal">#logo design</a>
+      <a href="#" class="font-normal">#brand design</a>
+      <a href="#" class="font-normal">#editorial design</a>
+      <a href="#" class="font-normal">#web design</a>
+    </div>
+  </div>
+  <div class="grid grid-col-1 lg:grid-cols-3 grid-row-1 gap-8 mt-16 mb-8">
     <div class="col-start-2 col-span-2 flex justify-between">
       <button class="text-2xl">&#8249;</button>
       <button class="text-2xl">&#8250;</button>
@@ -33,6 +41,10 @@
   background-image: url(http://placekitten.com/800/600);
   background-position: center;
   background-size: cover;
+}
+
+.tags a {
+  color: #9A9A9A;
 }
 
 @media only screen and (min-width: 1441px) {
