348d8d34cc3ec1ee24283c70189d500145c43211
szboynono <szboylbd@hotmail.com>
2020-10-02 14:48:19 -0400

    rough out the about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,17 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about grid grid-col-1 lg:grid-cols-3 grid-row-5 gap-8">
+    <div class="col-start-2 col-span-2 text-3xl font-normal border-b-2 border-gray-900 pb-8">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet
+      reprehenderit odit dolorum neque incidunt, suscipit corrupti nobis
+      distinctio sequi, blanditiis error deleniti eveniet eos culpa sint magnam
+      expedita unde laborum. Lorem ipsum dolor sit amet consectetur adipisicing
+      elit. Corporis eaque fuga, voluptatibus perferendis sapiente deserunt vel
+      exercitationem suscipit ducimus voluptate voluptates vero dolor ipsam
+      inventore, quidem pariatur excepturi, molestiae ut?
+    </div>
+    <div class="col-start-2 col-span-2 border-b-2 border-gray-900 pb-8">EXPERIENCE</div>
+    <div class="col-start-2 col-span-2 border-b-2 border-gray-900 pb-8">AWARDS & RECONGNITION</div>
+    <div class="col-start-2 col-span-2 border-b-2 border-gray-900 pb-8">SELECT CLIENTS</div>
+    <div class="col-start-2 col-span-2 border-b-2 border-gray-900 pb-8">CONTACT</div>
   </div>
 </template>
