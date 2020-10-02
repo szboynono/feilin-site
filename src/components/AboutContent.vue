b98ad63ad96c216805a790cc3f44fd5e96a15166
szboynono <szboylbd@hotmail.com>
2020-10-02 16:12:53 -0400

    make the about content clickable

---
+++ src/components/AboutContent.vue
@@ -0,0 +1,28 @@
+<template>
+  <header @click="headerClick">
+    <slot name="header"></slot>
+  </header>
+  <div v-if="showContent" class="content border-t-2 border-gray-900 mt-8 font-normal">
+    <slot name="content"></slot>
+  </div>
+</template>
+
+<style lang="css">
+header {
+  cursor: pointer;
+}
+</style>
+
+<script>
+export default {
+  props: ['showContent', 'contentId'],
+  emits: ['title-clicked'],
+  setup(props, ctx) {
+    const headerClick = () => {
+      ctx.emit('title-clicked', props.contentId);
+    }
+  
+    return {headerClick}
+  }
+}
+</script>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="about grid grid-col-1 lg:grid-cols-3 grid-row-5 gap-8">
-    <div class="col-start-2 col-span-2 text-3xl font-normal border-b-2 border-gray-900 pb-8">
+    <div
+      class="col-start-2 col-span-2 text-3xl font-normal border-b-2 border-gray-900 pb-8"
+    >
       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet
       reprehenderit odit dolorum neque incidunt, suscipit corrupti nobis
       distinctio sequi, blanditiis error deleniti eveniet eos culpa sint magnam
@@ -9,9 +11,113 @@
       exercitationem suscipit ducimus voluptate voluptates vero dolor ipsam
       inventore, quidem pariatur excepturi, molestiae ut?
     </div>
-    <div class="col-start-2 col-span-2 border-b-2 border-gray-900 pb-8">EXPERIENCE</div>
-    <div class="col-start-2 col-span-2 border-b-2 border-gray-900 pb-8">AWARDS & RECONGNITION</div>
-    <div class="col-start-2 col-span-2 border-b-2 border-gray-900 pb-8">SELECT CLIENTS</div>
-    <div class="col-start-2 col-span-2 border-b-2 border-gray-900 pb-8">CONTACT</div>
+    <div class="col-start-2 col-span-2 border-b-2 border-gray-900 pb-8">
+      <about-content :showContent="contentState[0]" :contentId='0' @title-clicked="onTitleClick">
+        <template #header> EXPERIENCE </template>
+        <template #content>
+          <div class="mt-8">
+            <p>Freelance - 2017 - Present</p>
+            <p>Senior Designer</p>
+          </div>
+          <div class="mt-8">
+            <p>Freelance - 2017 - Present</p>
+            <p>Senior Designer</p>
+          </div>
+          <div class="mt-8">
+            <p>Freelance - 2017 - Present</p>
+            <p>Senior Designer</p>
+          </div>
+          <div class="mt-8">
+            <p>Freelance - 2017 - Present</p>
+            <p>Senior Designer</p>
+          </div>
+        </template>
+      </about-content>
+    </div>
+    <div class="col-start-2 col-span-2 border-b-2 border-gray-900 pb-8">
+      <about-content :showContent="contentState[1]" :contentId='1' @title-clicked="onTitleClick">
+        <template #header> AWARDS & RECONGNITION </template>
+        <template #content>
+          <div class="mt-8">
+            <p>Freelance - 2017 - Present</p>
+            <p>Senior Designer</p>
+          </div>
+          <div class="mt-8">
+            <p>Freelance - 2017 - Present</p>
+            <p>Senior Designer</p>
+          </div>
+          <div class="mt-8">
+            <p>Freelance - 2017 - Present</p>
+            <p>Senior Designer</p>
+          </div>
+          <div class="mt-8">
+            <p>Freelance - 2017 - Present</p>
+            <p>Senior Designer</p>
+          </div>
+        </template>
+      </about-content>
+    </div>
+    <div class="col-start-2 col-span-2 border-b-2 border-gray-900 pb-8">
+      <about-content :showContent="contentState[2]" :contentId='2' @title-clicked="onTitleClick">
+        <template #header> SELECT CLIENTS </template>
+        <template #content>
+          <div class="mt-8">
+            <p>Freelance - 2017 - Present</p>
+            <p>Senior Designer</p>
+          </div>
+          <div class="mt-8">
+            <p>Freelance - 2017 - Present</p>
+            <p>Senior Designer</p>
+          </div>
+          <div class="mt-8">
+            <p>Freelance - 2017 - Present</p>
+            <p>Senior Designer</p>
+          </div>
+          <div class="mt-8">
+            <p>Freelance - 2017 - Present</p>
+            <p>Senior Designer</p>
+          </div>
+        </template>
+      </about-content>
+    </div>
+    <div class="col-start-2 col-span-2 border-b-2 border-gray-900 pb-8">
+      <about-content :showContent="contentState[3]" :contentId='3' @title-clicked="onTitleClick">
+        <template #header> CONTACT </template>
+        <template #content>
+          <div class="mt-8">
+            <p>Freelance - 2017 - Present</p>
+            <p>Senior Designer</p>
+          </div>
+          <div class="mt-8">
+            <p>Freelance - 2017 - Present</p>
+            <p>Senior Designer</p>
+          </div>
+          <div class="mt-8">
+            <p>Freelance - 2017 - Present</p>
+            <p>Senior Designer</p>
+          </div>
+          <div class="mt-8">
+            <p>Freelance - 2017 - Present</p>
+            <p>Senior Designer</p>
+          </div>
+        </template>
+      </about-content>
+    </div>
   </div>
 </template>
+
+<script>
+import { ref } from 'vue';
+import AboutContent from "../components/AboutContent";
+
+export default {
+  components: {
+    AboutContent,
+  },
+  setup() {
+    const contentState = ref([false, false, false, false, false])
+    const onTitleClick = index => contentState.value[index] = !contentState.value[index];
+    return {contentState, onTitleClick}
+  }
+};
+</script>
