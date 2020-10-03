918271a5d80ff6eb1c51425a738c5cb9c89c9ffb
szboynono <szboylbd@hotmail.com>
2020-10-02 23:37:03 -0400

    fix footer

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,32 @@
 <template>
-<div class="m-8">
-  <div class="grid grid-flow-col grid-cols-3 gap-8 mb-8">
-    <h1><router-link to="/">FEILIN DESIGN</router-link></h1>
-    <router-link to="/work">WORK</router-link>
-    <router-link to="/about">ABOUT</router-link>
+  <div class="m-8 flex flex-col justify-between min-h-screen">
+    <div>
+      <div class="grid grid-flow-col grid-cols-3 gap-8 mb-8">
+        <h1><router-link to="/">FEILIN DESIGN</router-link></h1>
+        <router-link to="/work">WORK</router-link>
+        <router-link to="/about">ABOUT</router-link>
+      </div>
+      <router-view />
+    </div>
+    <div class="mt-8 footer">
+      <Footer></Footer>
+    </div>
   </div>
-  <router-view/>
-  <div class="mt-8 footer">
-    <Footer></Footer>
-  </div>
-</div>
 </template>
 
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Manrope:wght@200;300;400;500;600;700;800&display=swap');
+@import url("https://fonts.googleapis.com/css2?family=Manrope:wght@200;300;400;500;600;700;800&display=swap");
 #app {
-  font-family: 'Manrope', sans-serif;
+  font-family: "Manrope", sans-serif;
   font-weight: 800;
 }
 </style>
 
 <script>
-import Footer from './components/Footer';
+import Footer from "./components/Footer";
 export default {
   components: {
-    Footer
-  }
-}
+    Footer,
+  },
+};
 </script>
