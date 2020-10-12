c65027a0298be178444cddb37b1c6438c2b08991
szboynono <szboylbd@hotmail.com>
2020-10-12 16:51:00 -0400

    add assets for aurora

--- src/store/index.js
+++ src/store/index.js
@@ -22,12 +22,12 @@ const state = {
           alt: 'mattamy-work-3'
         },
         {
-          url: require("../assets/images/mattamy/poster.png"),
-          alt: 'mattamy-work-4'
-        },
-        {
           url: require("../assets/images/mattamy/card.png"),
           alt: 'mattamy-work-5'
+        },
+        {
+          url: require("../assets/images/mattamy/poster.png"),
+          alt: 'mattamy-work-4'
         },
         {
           url: require("../assets/images/mattamy/business-card.png"),
@@ -46,29 +46,41 @@ const state = {
     {
       id: "2",
       homeImg: {
-        url: require("../assets/images/mattamy/logo-2.png"),
-        alt: 'mattamy1'
+        url: require("../assets/images/aurora/poster-6.png"),
+        alt: 'aurora1'
       },
       workImgs: [
         {
-          url: require("../assets/images/mattamy/logo-2.png"),
-          alt: 'mattamy2'
+          url: require("../assets/images/aurora/logo-1.png"),
+          alt: 'aurora2'
         },
         {
-          url: require("../assets/images/mattamy/logo-collection.png"),
-          alt: 'mattamy3'
+          url: require("../assets/images/aurora/logo-2.gif"),
+          alt: 'aurora3'
         },
         {
-          url: require("../assets/images/mattamy/poster.png"),
-          alt: 'mattamy4'
+          url: require("../assets/images/aurora/logo-3.png"),
+          alt: 'aurora4'
         },
         {
-          url: require("../assets/images/mattamy/website-1.png"),
-          alt: 'mattamy5'
+          url: require("../assets/images/aurora/logo-4.png"),
+          alt: 'aurora5'
         },
         {
-          url: require("../assets/images/mattamy/website-2.png"),
-          alt: 'mattamy6'
+          url: require("../assets/images/aurora/business-card-5.png"),
+          alt: 'aurora6'
+        },
+        {
+          url: require("../assets/images/aurora/poster-6.png"),
+          alt: 'aurora7'
+        },
+        {
+          url: require("../assets/images/aurora/product-7.png"),
+          alt: 'aurora8'
+        },
+        {
+          url: require("../assets/images/aurora/pen-8.png"),
+          alt: 'aurora9'
         }
       ]
     },
